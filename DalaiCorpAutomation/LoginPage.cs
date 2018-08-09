c7762f63f58a9ef4602ae2a335c7cbac38f03e32
manasdalai <manas.dalai@yahoo.com>
2018-08-09 14:45:41 -0700

    Associated ChromeDriver

---
+++ DalaiCorpAutomation/LoginPage.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalaiCorpAutomation
+{
+    public class LoginPage
+    {
+        public static void GoTo()
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://aws.amazon.com/");
+        }
+    }
+}
---
+++ DalaiCorpTests/LoginTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DalaiCorpAutomation;
+
+namespace DalaiCorpTests
+{
+    [TestClass]
+    public class LoginTests
+    {
+        [TestMethod]
+        public void Admin_User_Can_Login()
+        {
+            LoginPage.GoTo();
+           // LoginPage.SignUpAs("Admin").WithPassword("Password").Login();
+            //Assert.IsTrue(DashBoardPage.IsAt, "Failed to Login");
+        }
+    }
+}
