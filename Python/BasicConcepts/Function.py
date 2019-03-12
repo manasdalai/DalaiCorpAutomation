be3e6bab6574fd7f1955f574a170f79ed4a13c27
manasdalai <mdalai@starbucks.com>
2019-03-12 16:01:01 -0700

    Initial Draft

--- Python/BasicConcepts/Function.py
+++ Python/BasicConcepts/Function.py
@@ -17,3 +17,45 @@ and print the results'''
 
 if round(.1 + .1 + .1, 10) == round(.3, 10):
     print("pass")
+
+'''Default Argument values'''
+
+
+def ask_ok(prompt, retries=4, reminder='Please try again!!!'):
+    while True:
+        ok = input(prompt)
+        if ok in ('y', 'ye', 'yes'):
+            return True
+        if ok in ('n', 'no', 'nop', 'nope'):
+            return False
+        retries = retries - 1
+        if retries < 0:
+            raise ValueError('Invalid user Response')
+        print(reminder)
+
+
+ask_ok('Do you really want to quit?')
+
+'''Lambda Expressions'''
+
+
+def make_incrementor(n):
+    return lambda x: x + n
+
+
+f = make_incrementor(42)
+print(f(0))
+print(f(10))
+
+'''Documentation String'''
+
+
+def documentation():
+    """Do nothing, but document it.
+
+    No really it does not do nothing
+    """
+    pass
+
+
+print(documentation.__doc__)
