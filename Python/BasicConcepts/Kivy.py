f4818f415e1c5657d7c05a7caae9582c88222776
manasdalai <mdalai@starbucks.com>
2019-03-13 20:59:45 -0700

    Initial Draft

--- Python/BasicConcepts/Kivy.py
+++ Python/BasicConcepts/Kivy.py
@@ -1,12 +1,18 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
 
 
 class SimpleKivy(App):
     def build(self):
         return Label(text="Hello Manas!!!")
-        return Label(text="Hello Manasa!!!")
+
+    def on_enter(instance, value):
+        print('User Press enter in', instance)
+
+    textinput = TextInput(text='Hello world', multiline=False)
+    textinput.bind(on_text_validate=on_enter)
 
 
 if __name__ == "__main__":
